bbdfde6cfed7d7917a71b570738aa255cc5c9a61
Amal <cmamal5@gmail.com>
2023-07-02 10:48:33 +0530

    url changes

---
+++ js/Handler.js
@@ -0,0 +1,20 @@
+function openurl(Pathname){
+var Url="";
+
+  switch(Pathname){
+        case "Linkedin":
+            Url="https://www.linkedin.com/in/amalmuralidharan5/";
+            break;
+        case "github":
+            Url="https://github.com/cmamal5";
+            break;
+        case "instagram":
+            Url="https://www.instagram.com/___ame_errante/";
+            break;
+        case "stack-overflow":
+            Url="https://stackoverflow.com/users/11957624/amal";
+            break;
+  }
+
+  window.open(Url);
+}
